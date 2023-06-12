e90fc0ab7dabf99db7d3bb4be835bc00566cbaee
Joaquín Solla Vázquez <joaquin.solla@udc.es>
2023-06-12 18:09:10 +0200

    Working on informacion.html

---
+++ scripts/slick.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.slick').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        swipe: false,
+        pauseOnFocus: false,
+    });
+});
